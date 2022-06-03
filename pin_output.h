5965dfc5154a9b63c8ad3c4358977e3f2bb00f12
Mads Bornebusch <mads.bornebusch@gmail.com>
2022-06-03 14:34:04 +0200

    Writing to the clocks from last to first

--- pin_output.h
+++ pin_output.h
@@ -1,8 +1,19 @@
 #define LBATT_PIN 19
 #define N_CLOCKS 48
 
+// // Seen from the back
+// const uint8_t pins[N_CLOCKS] PROGMEM = {
+// 2,  3,  4,  5,  6,  7,  8,  9,
+// 10, 11, 12, 13, 14, 15, 16, 17, 
+// 22, 23, 24, 25, 26, 27, 28, 29,
+// 30, 31, 32, 33, 34, 35, 36, 37,
+// 38, 39, 40, 41, 42, 43, 44, 45,
+// 46, 47, 48, 49, 50, 51, 52, 53
+// };
+
+
+// Seen from the front
 const uint8_t pins[N_CLOCKS] PROGMEM = {
-// 0
 9,  8,  7,  6,  5,  4,  3,  2,
 17, 16, 15, 14, 13, 12, 11, 10,
 29, 28, 27, 26, 25, 24, 23, 22, 
@@ -10,6 +21,7 @@ const uint8_t pins[N_CLOCKS] PROGMEM = {
 45, 44, 43, 42, 41, 40, 39, 38,
 53, 52, 51, 50, 49, 48, 47, 46
 };
+
 
 void turnOffPin(uint8_t pin_number){
   pinMode(pin_number, INPUT);
@@ -29,9 +41,9 @@ void setupOutput(void){
 void shiftOut(byte myDataOut, uint8_t latchon, uint8_t row) {
   for(uint8_t i = 0; i < 8; i++) {
     if(bitRead(myDataOut, i) == 1) {
-      turnOnPin(pins[row * 8 + i]);
+      turnOnPin(pins[(N_CLOCKS -1) - (row * 8 + i)]);
     } else {
-      turnOffPin(pins[row * 8 + i]);
+      turnOffPin(pins[(N_CLOCKS - 1) - (row * 8 + i)]);
     }
   }
 }
